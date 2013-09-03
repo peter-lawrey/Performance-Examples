e6d6de4d4f0d3a7d2a16b16726f43483360dcb40
Peter Lawrey <peter.lawrey@gmail.com>
2013-09-03 10:18:12 +0200

    Add PriceSerachMain

--- src/main/java/vanilla/java/perfeg/lambdas/PriceSearchMain.java
+++ src/main/java/vanilla/java/perfeg/lambdas/PriceSearchMain.java
@@ -16,43 +16,48 @@ public class PriceSearchMain {
 	static double minPrice, maxPrice;
 
 	public static void main(String[] args) {
-		int sampleSize = 10000;
-		generateProducts(sampleSize);
+		for (int sampleSize : new int[]{100_000, 30_000, 10_000, 3_000, 1_000, 300, 100, 30, 10, 3, 1}) {
+			generateProducts(sampleSize);
 
-		for (int i = 0; i < 30; i++) {
 			long time1 = 0;
 			long time2 = 0;
-			for (double window : new double[]{0.1, 0.5, 0.9}) {
-				minPrice = 0.5 - window / 2;
-				maxPrice = 0.5 + window / 2;
-				time1 += testLoopingFilter();
-				time2 += testFlitering();
-			}
-			System.out.printf("Time per entry was %,d and %,d ns %n",
-					time1 / sampleSize / 3, time2 / sampleSize / 3);
+			int count = 0;
+			long start = System.currentTimeMillis();
+			do {
+				for (double window : new double[]{0.1, 0.5, 0.9}) {
+					minPrice = 0.5 - window / 2;
+					maxPrice = 0.5 + window / 2;
+					time1 += testLoopingFilter();
+					time2 += testFlitering();
+					count++;
+				}
+			} while (System.currentTimeMillis() - start < 2e3);
+			System.out.printf("%,d: Average time per search was %,d and %,d ns %n",
+					sampleSize, time1 / count, time2 / count);
 		}
+	}
+
+	private static void generateProducts(int sampleSize) {
+		products.clear();
+		for (int i = 0; i < sampleSize; i++) {
+			products.add(new Product(Math.random()));
+		}
+	}
+
+	public static long testFlitering() {
+		long start = System.nanoTime();
+		filteredProducts = products.stream().filter(p -> p.price > minPrice).filter(p -> p.price < maxPrice).collect(Collectors.toList());
+		return System.nanoTime() - start;
 	}
 
 	public static long testLoopingFilter() {
 		long start = System.nanoTime();
-		List<Product> found = new ArrayList<Product>();
+		List<Product> found = new ArrayList<>();
 		for (Product product : products)
 			if (product.price > minPrice && product.price < maxPrice)
 				found.add(product);
 		filteredProducts = found;
 		return System.nanoTime() - start;
-	}
-
-	public static long testFlitering() {
-		long start = System.nanoTime();
-		filteredProducts = products.parallelStream().filter(p -> p.price > minPrice).filter(p -> p.price < maxPrice).collect(Collectors.toList());
-		return System.nanoTime() - start;
-	}
-
-	private static void generateProducts(int sampleSize) {
-		for (int i = 0; i < sampleSize; i++) {
-			products.add(new Product(Math.random()));
-		}
 	}
 
 	static class Product {
