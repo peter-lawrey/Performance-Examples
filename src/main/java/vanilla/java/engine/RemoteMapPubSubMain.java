4bc6b93a527198800dd4b6350f3deefbe956f41d
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-11-23 08:51:02 +0200

    Engine Examples

--- src/main/java/vanilla/java/engine/RemoteMapPubSubMain.java
+++ src/main/java/vanilla/java/engine/RemoteMapPubSubMain.java
@@ -5,17 +5,27 @@ import net.openhft.chronicle.engine.api.map.MapView;
 import net.openhft.chronicle.engine.api.pubsub.Reference;
 import net.openhft.chronicle.engine.api.pubsub.TopicPublisher;
 import net.openhft.chronicle.engine.api.tree.AssetTree;
+import net.openhft.chronicle.engine.server.ServerEndpoint;
 import net.openhft.chronicle.engine.tree.VanillaAssetTree;
 import net.openhft.chronicle.network.TCPRegistry;
+import net.openhft.chronicle.wire.WireType;
+import net.openhft.chronicle.wire.YamlLogging;
 
 import java.io.IOException;
 
 /**
  * Created by peter.lawrey on 23/11/2015.
  */
-public class MapPubSubMain {
+public class RemoteMapPubSubMain {
     public static void main(String[] args) throws IOException {
-        AssetTree tree = new VanillaAssetTree("client").forTesting();
+        YamlLogging.setAll(true);
+        TCPRegistry.createServerSocketChannelFor("test.host.port");
+
+        AssetTree remoteTree = new VanillaAssetTree("test").forServer();
+        ServerEndpoint serverEndpoint = new ServerEndpoint("test.host.port", remoteTree, WireType.TEXT);
+
+
+        AssetTree tree = new VanillaAssetTree("client").forRemoteAccess("test.host.port", WireType.TEXT);
         MapView<String, String> map = tree.acquireMap("/group/map", String.class, String.class);
         map.put("one", "hello");
         map.put("two", "bye");
