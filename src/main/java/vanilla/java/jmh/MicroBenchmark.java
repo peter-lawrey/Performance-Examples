a0f4fb1ecdb8a4732cfff6ecfbef1511a24d57b1
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-11-26 16:03:44 +0200

    Latency test

--- src/main/java/vanilla/java/jmh/MicroBenchmark.java
+++ src/main/java/vanilla/java/jmh/MicroBenchmark.java
@@ -1,15 +1,40 @@
 package vanilla.java.jmh;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * Created by peter.lawrey on 26/11/2015.
  */
+@State(Scope.Thread)
 public class MicroBenchmark {
-    @Benchmark
-    public void testMe() {
-        for (int i = 0; i < 1000; i++) {
+    public static void main(String[] args) throws RunnerException {
+        long time = 10;
+        Options opt = new OptionsBuilder()
+                .include(MicroBenchmark.class.getSimpleName())
+                .warmupIterations(6)
+                .forks(1)
+                .mode(Mode.SampleTime)
+                .measurementTime(TimeValue.seconds(time))
+                .timeUnit(TimeUnit.MICROSECONDS)
+                .build();
 
-        }
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    public long testMe() {
+        LockSupport.parkNanos(1);
+        return 0;
     }
 }
