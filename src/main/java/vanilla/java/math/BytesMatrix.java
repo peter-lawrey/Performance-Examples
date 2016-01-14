80d8017909f92abc9049e33ff9ee5f49d94cc118
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-01-14 10:34:48 -0500

    Native memory matrix

--- src/main/java/vanilla/java/math/BytesMatrix.java
+++ src/main/java/vanilla/java/math/BytesMatrix.java
@@ -6,20 +6,23 @@ import net.openhft.chronicle.bytes.NativeBytesStore;
  * Created by peter.lawrey on 14/01/2016.
  */
 public class BytesMatrix {
-    private static final long ROWS_OFFSET = 0L;
-    private static final long COLUMNS_OFFSET = ROWS_OFFSET + Integer.SIZE;
+    private static final long MAX_ROWS_OFFSET = 0L;
+    private static final long USED_ROWS_OFFSET = MAX_ROWS_OFFSET + Integer.SIZE;
+    private static final long COLUMNS_OFFSET = USED_ROWS_OFFSET + Integer.SIZE;
     private static final long HEADER_SIZE = COLUMNS_OFFSET + Integer.SIZE;
 
     private final NativeBytesStore<Void> bytes;
-    private final int rows;
+    private final int maxRows;
     private final int columns;
+    private int rowsUsed = 0;
 
-    public BytesMatrix(int rows, int columns) {
-        this.rows = rows;
+    public BytesMatrix(int maxRows, int columns) {
+        this.maxRows = maxRows;
         this.columns = columns;
-        long size = calcSize(rows, columns);
-        bytes = NativeBytesStore.nativeStoreWithFixedCapacity(size);
-        bytes.writeInt(ROWS_OFFSET, rows);
+        long size = calcSize(maxRows, columns);
+        bytes = NativeBytesStore.lazyNativeBytesStoreWithFixedCapacity(size);
+        bytes.writeInt(MAX_ROWS_OFFSET, maxRows);
+        bytes.writeInt(USED_ROWS_OFFSET, 0);
         bytes.writeInt(COLUMNS_OFFSET, columns);
     }
 
@@ -42,8 +45,12 @@ public class BytesMatrix {
     }
 
     private long index(int row, int column) {
-        assert row < 0 || row >= rows;
+        assert row < 0 || row >= maxRows;
         assert column < 0 || column >= columns;
+        if (row >= rowsUsed) {
+            bytes.zeroOut(rowsUsed * columns * Double.SIZE, (row + 1) * columns * Double.SIZE);
+            bytes.writeInt(USED_ROWS_OFFSET, rowsUsed = row + 1);
+        }
         return HEADER_SIZE + (row * columns + column) * Double.SIZE;
     }
 }
