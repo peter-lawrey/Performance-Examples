8a39b211ac28a35cf763c1b1440c21e56f3febd8
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-04-02 19:55:47 +0100

    Add changing the class of an object example.

---
+++ src/main/java/vanilla/java/unsafe/UnsafeIdentityDemo.java
@@ -0,0 +1,37 @@
+package vanilla.java.unsafe;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by peter on 24/11/2014.
+ */
+public class UnsafeIdentityDemo {
+
+    static final Unsafe UNSAFE;
+
+    static {
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            UNSAFE = (Unsafe) theUnsafe.get(null);
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    public static void setIdentityHashCode(Object o, int code) {
+        UNSAFE.putInt(o, 1l, code & 0x7FFF_FFF);
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Double d = 1.0;
+        Double d2 = 1.0;
+        setIdentityHashCode(d, 1);
+        setIdentityHashCode(d2, 1);
+        System.out.println("d: " + d + " System.identityHashCode(d): " + System.identityHashCode(d));
+        System.out.println("d2: " + d2 + " System.identityHashCode(d2): " + System.identityHashCode(d2));
+        System.out.println("d == d2: " + (d == d2));
+    }
+}
