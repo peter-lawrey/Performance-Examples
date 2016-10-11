c4f66b97f1f2a7e37200c2a8bf379231fc55bef0
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-10-11 15:13:15 +0100

    Add how many objects example

--- src/main/java/vanilla/java/objects/HowManyObjectsMain.java
+++ src/main/java/vanilla/java/objects/HowManyObjectsMain.java
@@ -1,16 +1,17 @@
-package vanilla.java.references;
+package vanilla.java.objects;
 
 import java.io.IOException;
-import java.util.stream.IntStream;
 
 /**
  * Created by peter.lawrey on 23/11/2015.
  */
 public class HowManyObjectsMain {
     public static void main(String[] args) throws IOException {
-        IntStream.range(0, 10)
-                .distinct()
-                .forEach(System.out::println);
+        String a = "hello";
+        String b = "world";
+        String c = a + ' ' + b;
+        System.out.println(c);
+
         System.in.read();
     }
 }
