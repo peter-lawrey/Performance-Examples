3fe60c685eb59eb307e4e88dacbdaa314b5a525d
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-10-11 15:51:13 +0100

    Add CleanerMain example

--- src/main/java/vanilla/java/objects/CleanerMain.java
+++ src/main/java/vanilla/java/objects/CleanerMain.java
@@ -2,14 +2,20 @@ package vanilla.java.objects;
 
 import sun.misc.Cleaner;
 
+import java.util.RandomAccess;
+
 /**
  * Created by peter on 11/10/2016.
  */
 public class CleanerMain {
     public static void main(String[] args) throws InterruptedException {
+        new RandomAccess() {
+            public void finalize() {
+                new Throwable(Thread.currentThread().getName()).printStackTrace();
+            }
+        };
         Cleaner.create(new Integer(1), () ->
                 new Throwable(Thread.currentThread().getName()).printStackTrace());
-
         System.gc();
         Thread.sleep(1000);
     }
